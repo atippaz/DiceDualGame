4e101ae4b0e14d55f16071a690e51732dec64131
atippaz <nasakun13201@gmail.com>
2023-10-18 09:31:25 -0700

    update socket problem

--- dice-dual-game/src/api/socket/index.ts
+++ dice-dual-game/src/api/socket/index.ts
@@ -1,7 +1,12 @@
 import { io, type Socket } from 'socket.io-client'
 const path = import.meta.env.VITE_API_GAME_SERVER || ''
 export default () => {
-    const socket: Socket = io(path)
+    const socket: Socket = io(path, {
+        secure: true,
+        rejectUnauthorized: false,
+        path: "/socket.io/",
+        transports: ["websocket"]
+    })
     return {
         socket,
     }
