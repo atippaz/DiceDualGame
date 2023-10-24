0badb714df9e9d6deb551c88a5e9af6c03f2ef67
atippaz <nasakun13201@gmail.com>
2023-10-24 17:45:48 +0700

    update and fix bug

--- dice-dual-server/ecosystem.config.cjs
+++ dice-dual-server/ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'serverapi',
-            script: 'index.js',
+            script: 'server.js',
             instances: 1,
             autorestart: true,
             watch: true,
